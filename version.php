5a0e49424a6c7aee103502d1ac5056cefaa663db
Anthony Kuske <anthonykuske@gmail.com>
2015-03-13 22:43:29 +0000

    Update version.php

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015031301; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2015031302; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2012112900; // Requires this Moodle version
 $plugin->component = 'block_search'; // Full name of the plugin (used for diagnostics).
