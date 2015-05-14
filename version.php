f82ab2c16102bfb6799af92aea6db1dda511f8c3
Anthony Kuske <anthonykuske@gmail.com>
2015-05-14 17:56:22 +0200

    Fix typo

--- version.php
+++ version.php
@@ -27,3 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2015051401; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2013111800; // MOODLE_26_STABLE
 $plugin->component = 'block_search'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
